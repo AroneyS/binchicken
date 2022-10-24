0a6ed7bfca94e6c3c35c468ec39827b49734a05d
AroneyS <samuel.aroney@qut.edu.au>
2022-10-24 14:23:19 +1000

    refactor query_processing to reduce memory usage

--- cockatoo/workflow/scripts/query_processing.py
+++ cockatoo/workflow/scripts/query_processing.py
@@ -5,43 +5,65 @@
 
 import pandas as pd
 
-SEQUENCE_IDENTITY = snakemake.params.sequence_identity
-WINDOW_SIZE = snakemake.params.window_size
+def processing(
+    query_read,
+    pipe_read,
+    SEQUENCE_IDENTITY=0.86,
+    WINDOW_SIZE=60):
 
-################
-### Pipeline ###
-################
-queries = []
-for read in snakemake.input.query_reads:
-    queries.append(pd.read_csv(read, sep = "\t"))
-query_sequences = pd.concat(queries)
+    appraised = (query_read
+        # Rename to match appraise output
+        # Query output: query_name, query_sequence, divergence, num_hits, coverage, sample, marker, hit_sequence, taxonomy
+        # Appraise output: gene, sample, sequence, num_hits, coverage, taxonomy, found_in
+        .rename(columns = {"marker": "gene", "query_name":"sample", "query_sequence": "sequence", "sample": "found_in"})
+        # Query taxonomy, num_hits and coverage are from database (e.g. the genomes)
+        .drop(["taxonomy", "num_hits", "coverage"], axis = 1)
+        .set_index(["gene", "sample", "sequence"])
+        .join(pipe_read.set_index(["gene", "sample", "sequence"]), on = ["gene", "sample", "sequence"])
+        .reset_index()
+        # Join query sample column to create found_in column
+        .groupby(["gene", "sample", "sequence", "num_hits", "coverage", "taxonomy", "divergence"])["found_in"]
+        .agg(lambda x: ",".join(sorted(x)))
+        .reset_index()
+        )
 
-pipes = []
-for read in snakemake.input.pipe_reads:
-    pipes.append(pd.read_csv(read, sep = "\t"))
-pipe_sequences = pd.concat(pipes)
+    # Split dataframe into binned/unbinned
+    appraised["binned"] = appraised["divergence"].apply(lambda x: x <= (1 - SEQUENCE_IDENTITY) * WINDOW_SIZE)
+    binned = appraised[appraised["binned"]].drop(["divergence", "binned"], axis = 1)
+    unbinned = appraised[~appraised["binned"]].drop(["divergence", "binned"], axis = 1)
+    unbinned["found_in"] = None
 
-appraised = (query_sequences
-    # Rename to match appraise output
-    # Query output: query_name, query_sequence, divergence, num_hits, coverage, sample, marker, hit_sequence, taxonomy
-    # Appraise output: gene, sample, sequence, num_hits, coverage, taxonomy, found_in
-    .rename(columns = {"marker": "gene", "query_name":"sample", "query_sequence": "sequence", "sample": "found_in"})
-    # Query taxonomy, num_hits and coverage are from database (e.g. the genomes)
-    .drop(["taxonomy", "num_hits", "coverage"], axis = 1)
-    .set_index(["gene", "sample", "sequence"])
-    .join(pipe_sequences.set_index(["gene", "sample", "sequence"]), on = ["gene", "sample", "sequence"])
-    .reset_index()
-    # Join query sample column to create found_in column
-    .groupby(["gene", "sample", "sequence", "num_hits", "coverage", "taxonomy", "divergence"])["found_in"]
-    .agg(lambda x: ",".join(sorted(x)))
-    .reset_index()
-    )
+    return binned, unbinned
 
-# Split dataframe into binned/unbinned
-appraised["binned"] = appraised["divergence"].apply(lambda x: x <= (1 - SEQUENCE_IDENTITY) * WINDOW_SIZE)
-binned = appraised[appraised["binned"]].drop(["divergence", "binned"], axis = 1)
-unbinned = appraised[~appraised["binned"]].drop(["divergence", "binned"], axis = 1)
-unbinned["found_in"] = None
+def pipeline(
+    query_reads,
+    pipe_reads,
+    SEQUENCE_IDENTITY=0.86,
+    WINDOW_SIZE=60):
 
-binned.to_csv(snakemake.output.binned, sep="\t", index=False)
-unbinned.to_csv(snakemake.output.unbinned, sep="\t", index=False)
+    for query, pipe in zip(query_reads, pipe_reads):
+        binned, unbinned = processing(
+            pd.read_csv(query, sep = "\t"),
+            pd.read_csv(pipe, sep = "\t"),
+            SEQUENCE_IDENTITY,
+            WINDOW_SIZE)
+        yield binned, unbinned
+
+if __name__ == "__main__":
+    SEQUENCE_IDENTITY = snakemake.params.sequence_identity
+    WINDOW_SIZE = snakemake.params.window_size
+    query_reads = snakemake.input.query_reads
+    pipe_reads = snakemake.input.pipe_reads
+
+    outputs = pipeline(
+        query_reads,
+        pipe_reads,
+        SEQUENCE_IDENTITY=SEQUENCE_IDENTITY,
+        WINDOW_SIZE=WINDOW_SIZE
+        )
+
+    first = True
+    for binned, unbinned in outputs:
+        binned.to_csv(snakemake.output.binned, sep = "\t", mode = "a", header = first, index = False)
+        unbinned.to_csv(snakemake.output.unbinned, sep = "\t", mode = "a", header = first, index = False)
+        first = False
