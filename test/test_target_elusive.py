2d3a72880ae8c1d17c2a85601aaf71c0dd4bc2be
AroneyS <samuel.aroney@qut.edu.au>
2022-11-16 14:38:16 +1000

    add more tests for target_elusive.py

--- test/test_target_elusive.py
+++ test/test_target_elusive.py
@@ -23,19 +23,131 @@ class Tests(unittest.TestCase):
 
     def test_target_elusive(self):
         unbinned = pd.DataFrame([
-            ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", ""],
-            ["S3.1", "sample_2.1", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", ""],
         ], columns=APPRAISE_COLUMNS)
 
         expected_targets = pd.DataFrame([
-            ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", "0"],
-            ["S3.1", "sample_2.1", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", "0"],
         ], columns=TARGETS_COLUMNS).astype({"target": object})
         expected_edges = pd.DataFrame([
-            ["Root", 1, "0", "sample_1.1", "sample_2.1"],
+            ["Root", 1, "0", "sample_1", "sample_2"],
         ], columns=EDGES_COLUMNS)
 
         observed_targets, observed_edges = pipeline(unbinned)
+        self.assertDataFrameEqual(expected_targets, observed_targets)
+        self.assertEdgesDfEqual(expected_edges, observed_edges)
+
+    def test_target_elusive_low_coverage(self):
+        unbinned = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 5, "Root", ""],
+            ["S3.1", "sample_2", "AAA", 5, 4, "Root", ""],
+        ], columns=APPRAISE_COLUMNS)
+
+        expected_targets = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 5, "Root", "0"],
+            ["S3.1", "sample_2", "AAA", 5, 4, "Root", "0"],
+        ], columns=TARGETS_COLUMNS).astype({"target": object})
+        expected_edges = pd.DataFrame([
+        ], columns=EDGES_COLUMNS).astype({"weight": int})
+
+        observed_targets, observed_edges = pipeline(unbinned)
+        observed_edges.index = []
+        self.assertDataFrameEqual(expected_targets, observed_targets)
+        self.assertEdgesDfEqual(expected_edges, observed_edges)
+
+    def test_target_elusive_two_targets(self):
+        unbinned = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_1", "AAB", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAB", 5, 10, "Root", ""],
+        ], columns=APPRAISE_COLUMNS)
+
+        expected_targets = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_1", "AAB", 5, 10, "Root", "1"],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_2", "AAB", 5, 10, "Root", "1"],
+        ], columns=TARGETS_COLUMNS).astype({"target": object})
+        expected_edges = pd.DataFrame([
+            ["Root", 2, "0,1", "sample_1", "sample_2"],
+        ], columns=EDGES_COLUMNS)
+
+        observed_targets, observed_edges = pipeline(unbinned)
+        self.assertDataFrameEqual(expected_targets, observed_targets)
+        self.assertEdgesDfEqual(expected_edges, observed_edges)
+
+    def test_target_elusive_multiple_genes(self):
+        unbinned = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", ""],
+            ["S3.2", "sample_1", "AAB", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", ""],
+            ["S3.2", "sample_2", "AAB", 5, 10, "Root", ""],
+        ], columns=APPRAISE_COLUMNS)
+
+        expected_targets = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", "0"],
+            ["S3.2", "sample_1", "AAB", 5, 10, "Root", "1"],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", "0"],
+            ["S3.2", "sample_2", "AAB", 5, 10, "Root", "1"],
+        ], columns=TARGETS_COLUMNS).astype({"target": object})
+        expected_edges = pd.DataFrame([
+            ["Root", 2, "0,1", "sample_1", "sample_2"],
+        ], columns=EDGES_COLUMNS)
+
+        observed_targets, observed_edges = pipeline(unbinned)
+        self.assertDataFrameEqual(expected_targets, observed_targets)
+        self.assertEdgesDfEqual(expected_edges, observed_edges)
+
+    def test_target_elusive_three_sample_mix(self):
+        unbinned = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_1", "AAB", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAB", 5, 10, "Root", ""],
+            ["S3.1", "sample_3", "AAA", 5, 10, "Root", ""],
+            ["S3.1", "sample_3", "AAC", 5, 10, "Root", ""],
+        ], columns=APPRAISE_COLUMNS)
+
+        expected_targets = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_1", "AAB", 5, 10, "Root", "1"],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_2", "AAB", 5, 10, "Root", "1"],
+            ["S3.1", "sample_3", "AAA", 5, 10, "Root", "0"],
+            ["S3.1", "sample_3", "AAC", 5, 10, "Root", "2"],
+        ], columns=TARGETS_COLUMNS).astype({"target": object})
+        expected_edges = pd.DataFrame([
+            ["Root", 2, "0,1", "sample_1", "sample_2"],
+            ["Root", 1, "0", "sample_1", "sample_3"],
+            ["Root", 1, "0", "sample_2", "sample_3"],
+        ], columns=EDGES_COLUMNS)
+
+        observed_targets, observed_edges = pipeline(unbinned)
+        self.assertDataFrameEqual(expected_targets, observed_targets)
+        self.assertEdgesDfEqual(expected_edges, observed_edges)
+
+    def test_target_elusive_target_taxa(self):
+        unbinned = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root; d__Bacteria; p__Planctomycetota", ""],
+            ["S3.1", "sample_1", "AAB", 5, 10, "Root", ""],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root; d__Bacteria; p__Planctomycetota", ""],
+            ["S3.1", "sample_2", "AAB", 5, 10, "Root", ""],
+        ], columns=APPRAISE_COLUMNS)
+
+        expected_targets = pd.DataFrame([
+            ["S3.1", "sample_1", "AAA", 5, 10, "Root; d__Bacteria; p__Planctomycetota", "0"],
+            ["S3.1", "sample_1", "AAB", 5, 10, "Root", "1"],
+            ["S3.1", "sample_2", "AAA", 5, 10, "Root; d__Bacteria; p__Planctomycetota", "0"],
+            ["S3.1", "sample_2", "AAB", 5, 10, "Root", "1"],
+        ], columns=TARGETS_COLUMNS).astype({"target": object})
+        expected_edges = pd.DataFrame([
+            ["p__Planctomycetota", 1, "0", "sample_1", "sample_2"],
+        ], columns=EDGES_COLUMNS)
+
+        observed_targets, observed_edges = pipeline(unbinned, TAXA_OF_INTEREST="p__Planctomycetota")
         self.assertDataFrameEqual(expected_targets, observed_targets)
         self.assertEdgesDfEqual(expected_edges, observed_edges)
 
