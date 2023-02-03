f79bb905f574d7e01a58bd9bf69dd34bd79274e2
AroneyS <samuel.aroney@qut.edu.au>
2023-02-03 10:33:31 +1000

    account for duplicate binned sequences in coassembled samples

--- cockatoo/workflow/scripts/evaluate.py
+++ cockatoo/workflow/scripts/evaluate.py
@@ -51,6 +51,7 @@ def evaluate(unbinned_otu_table, binned_otu_table, elusive_clusters, elusive_edg
         .join(elusive_clusters)
         .dropna(subset=["coassembly"])
         .set_index(["coassembly", "gene", "sequence"])
+        .drop_duplicates()
         )
     nontarget_otu_table["target"] = None
 
--- test/test_evaluate_script.py
+++ test/test_evaluate_script.py
@@ -165,6 +165,39 @@ class Tests(unittest.TestCase):
         self.assertDataFrameEqual(expected_matches, observed_matches)
         self.assertDataFrameEqual(expected_unmatched, observed_unmatched)
 
+    def test_evaluate_script_samples_with_same_sequence(self):
+        targets = pd.DataFrame([
+            ["S3.1", "sample_1.1", "AAA", 5, 10.0, "Root; old", 10],
+            ["S3.1", "sample_2.1", "AAA", 5, 10.0, "Root; old", 10],
+        ], columns=TARGET_COLUMNS)
+        nontargets = pd.DataFrame([
+            ["S3.1", "sample_1.1", "BBB", 5, 10.0, "Root; old", "oldgenome_1"],
+            ["S3.1", "sample_2.1", "BBB", 5, 10.0, "Root; old", "oldgenome_1"],
+        ], columns=APPRAISE_COLUMNS)
+        clusters = pd.DataFrame([
+            ["sample_1,sample_2", 2, 1, 1, 100, "sample_1,sample_2,sample_3", "coassembly_0"],
+        ], columns=CLUSTER_COLUMNS)
+        edges = pd.DataFrame([
+            ["Root", 1, "10", "sample_1.1", "sample_2.1"],
+        ], columns=EDGE_COLUMNS)
+        recovered = pd.DataFrame([
+            ["S3.1", "coassembly_0-genome_1_transcripts", "AAA", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_0-genome_1_transcripts", "AAB", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_0-genome_1_transcripts", "BBB", 1, 2.0, "Root"],
+        ], columns=SINGLEM_COLUMNS)
+
+        expected_matches = pd.DataFrame([
+            ["coassembly_0", "S3.1", "AAA", "genome_1_transcripts", "10", None, "Root"],
+            ["coassembly_0", "S3.1", "BBB", "genome_1_transcripts", None, "oldgenome_1", "Root"],
+        ], columns=OUTPUT_COLUMNS)
+        expected_unmatched = pd.DataFrame([
+            ["coassembly_0", "S3.1", "AAB", "genome_1_transcripts", None, None, "Root"],
+        ], columns=OUTPUT_COLUMNS)
+
+        observed_matches, observed_unmatched = evaluate(targets, nontargets, clusters, edges, recovered)
+        self.assertDataFrameEqual(expected_matches, observed_matches)
+        self.assertDataFrameEqual(expected_unmatched, observed_unmatched)
+
 
 if __name__ == '__main__':
     unittest.main()
