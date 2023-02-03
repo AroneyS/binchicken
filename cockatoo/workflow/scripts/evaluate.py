5964a18cd132d1a51ae1063d2e738b86cbbf55e1
AroneyS <samuel.aroney@qut.edu.au>
2023-02-03 10:14:19 +1000

    add binned sequence matching to evaluate.py enables true-novel sequences to be distinguished

--- cockatoo/workflow/scripts/evaluate.py
+++ cockatoo/workflow/scripts/evaluate.py
@@ -6,9 +6,9 @@
 import pandas as pd
 import re
 
-def evaluate(unbinned_otu_table, elusive_clusters, elusive_edges, recovered_otu_table):
+def evaluate(unbinned_otu_table, binned_otu_table, elusive_clusters, elusive_edges, recovered_otu_table):
 
-    output_columns = ["coassembly", "gene", "sequence", "genome", "target", "taxonomy"]
+    output_columns = ["coassembly", "gene", "sequence", "genome", "target", "found_in", "taxonomy"]
 
     if len(recovered_otu_table) == 0:
         empty_output = pd.DataFrame(columns=output_columns)
@@ -23,8 +23,8 @@ def evaluate(unbinned_otu_table, elusive_clusters, elusive_edges, recovered_otu_
     elusive_clusters["samples"] = elusive_clusters["samples"].str.split(",")
     elusive_clusters = elusive_clusters.explode("samples").set_index("samples")[["coassembly"]]
 
-    elusive_edges["sample1"] = elusive_edges["sample1"].apply(lambda x: re.sub("\.1$", "", x))
-    elusive_edges["sample2"] = elusive_edges["sample2"].apply(lambda x: re.sub("\.1$", "", x))
+    elusive_edges["sample1"] = elusive_edges["sample1"].apply(lambda x: re.sub(r"\.1$", "", x))
+    elusive_edges["sample2"] = elusive_edges["sample2"].apply(lambda x: re.sub(r"\.1$", "", x))
     elusive_edges = (elusive_edges
         .set_index("sample1")
         .join(elusive_clusters)
@@ -42,19 +42,34 @@ def evaluate(unbinned_otu_table, elusive_clusters, elusive_edges, recovered_otu_
 
     # Create otu table with original sequence, cluster id, target id and associated coassemblies
     elusive_otu_table = coassembly_edges.join(unbinned_otu_table.set_index("target")).reset_index().set_index(["coassembly", "gene", "sequence"])
+    elusive_otu_table["found_in"] = None
 
-    # Load recovered otu table and join elusive sequences
+    # Add binned otu table to above with target NA
+    binned_otu_table["sample"] = binned_otu_table["sample"].apply(lambda x: re.sub(r"\.1$", "", x))
+    nontarget_otu_table = (binned_otu_table
+        .set_index(["sample"])[["gene", "sequence", "taxonomy", "found_in"]]
+        .join(elusive_clusters)
+        .dropna(subset=["coassembly"])
+        .set_index(["coassembly", "gene", "sequence"])
+        )
+    nontarget_otu_table["target"] = None
+
+    haystack_otu_table = pd.concat([elusive_otu_table, nontarget_otu_table])
+
+    # Load recovered otu table and join elusive and nontarget sequences
     recovered_otu_table[["coassembly", "genome"]] = recovered_otu_table["sample"].str.split("-", n=1, expand=True)
     recovered_coassemblies = set(recovered_otu_table["coassembly"])
 
-    combined_otu_table = recovered_otu_table.set_index(["coassembly", "gene", "sequence"])[["genome", "taxonomy"]].join(elusive_otu_table, how="outer", rsuffix="old").reset_index()
+    combined_otu_table = recovered_otu_table.set_index(["coassembly", "gene", "sequence"])[["genome", "taxonomy"]].join(haystack_otu_table, how="outer", rsuffix="old").reset_index()
     combined_otu_table["taxonomy"] = combined_otu_table["taxonomy"].combine(combined_otu_table["taxonomyold"], lambda a,b: a if not pd.isna(a) else b)
     combined_otu_table = combined_otu_table.drop("taxonomyold", axis=1)
     combined_otu_table.insert(len(combined_otu_table.columns)-1, "taxonomy", combined_otu_table.pop("taxonomy"))
     combined_otu_table = combined_otu_table[combined_otu_table["coassembly"].isin(recovered_coassemblies)]
+    # Choose sequences where genome is present (from recovered) and/or target is present (from unbinned targets)
+    combined_otu_table = combined_otu_table[combined_otu_table["genome"].notnull() | combined_otu_table["target"].notnull()]
 
-    matches = combined_otu_table.dropna(subset = ["target"]).reset_index(drop=True)
-    unmatched = combined_otu_table[combined_otu_table["target"].isnull()].reset_index(drop=True)
+    matches = combined_otu_table.dropna(subset=["target", "found_in"], how="all").reset_index(drop=True)
+    unmatched = combined_otu_table[combined_otu_table["target"].isnull() & combined_otu_table["found_in"].isnull()].reset_index(drop=True)
 
     return matches, unmatched
 
