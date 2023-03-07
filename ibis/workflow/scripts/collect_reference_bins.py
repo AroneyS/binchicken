83c126954359efe0b6c3a339afe1d466ac675dd5
AroneyS <samuel.aroney@qut.edu.au>
2023-03-07 14:48:41 +1000

    switch collect_reference_bins to polars

--- ibis/workflow/scripts/collect_reference_bins.py
+++ ibis/workflow/scripts/collect_reference_bins.py
@@ -3,7 +3,8 @@
 #################################
 # Author: Samuel Aroney
 
-import pandas as pd
+import polars as pl
+import os
 import numpy as np
 import extern
 
@@ -15,40 +16,63 @@ def trimmed_mean(data, trim=0.1):
         a = sorted(data)
         return np.mean(a[cut:-cut])
 
-def pipeline(appraise_binned, appraise_unbinned, sample, MIN_APPRAISED=0.1):
-    appraise_binned["sample"] = appraise_binned["sample"].str.replace("\.1$", "", regex=True)
-    appraise_binned = appraise_binned[appraise_binned["sample"] == sample]
+def pipeline(appraise_binned, appraise_unbinned, sample, MIN_APPRAISED=0.1, TRIM_FRACTION=0.1):
+    print(f"Polars using {str(pl.threadpool_size())} threads")
 
-    appraise_unbinned["sample"] = appraise_unbinned["sample"].str.replace("\.1$", "", regex=True)
-    appraise_unbinned = appraise_unbinned[appraise_unbinned["sample"] == sample]
+    appraise_binned = appraise_binned.with_columns(
+        pl.col("sample").str.replace("\.1$", "")
+    ).filter(
+        pl.col("sample") == sample
+    )
 
-    num_binned = sum(appraise_binned["num_hits"].to_list())
-    num_unbinned = sum(appraise_unbinned["num_hits"].to_list())
+    appraise_unbinned = appraise_unbinned.with_columns(
+        pl.col("sample").str.replace("\.1$", "")
+    ).filter(
+        pl.col("sample") == sample
+    )
+
+    num_binned = sum(appraise_binned.get_column("num_hits").to_list())
+    num_unbinned = sum(appraise_unbinned.get_column("num_hits").to_list())
     perc_binned = num_binned / (num_binned + num_unbinned)
 
     if perc_binned < MIN_APPRAISED:
         return set()
 
-    appraise_binned["found_in"] = appraise_binned["found_in"].str.split(",")
-    appraise_binned = appraise_binned.explode("found_in")
-    appraise_binned["found_in"] = appraise_binned["found_in"].str.replace("_protein$", "", regex=True)
+    reference_bins = appraise_binned.with_columns(
+        pl.col("found_in").str.split(",")
+    ).explode(
+        "found_in"
+    ).with_columns(
+        pl.col("found_in").str.replace("_protein$", "")
+    ).groupby(
+        ["gene", "found_in"]
+    ).agg(
+        pl.col("coverage").sum()
+    ).pivot(
+        values="coverage", index="gene", columns="found_in", 
+    ).melt(
+        id_vars="gene", variable_name="found_in", value_name="coverage"
+    ).fill_null(0
+    ).groupby(
+        "found_in"
+    ).agg(
+        (pl.col("coverage").len() * TRIM_FRACTION).floor().cast(int).alias("cut"),
+        pl.col("coverage")
+    ).with_columns(
+        pl.col("coverage").arr.sort().arr.slice(
+            pl.col("cut"), pl.col("coverage").arr.lengths() - 2 * pl.col("cut")
+            ).arr.mean()
+    ).filter(
+        pl.col("coverage") > 0
+    ).get_column("found_in"
+    ).to_list()
 
-    trimmed_binned = (appraise_binned.groupby(["gene", "found_in"])["coverage"]
-        .sum()
-        .reset_index()
-        .pivot(index="gene", columns="found_in", values="coverage")
-        .reset_index()
-        .melt(id_vars="gene")
-        .fillna(0)
-        .groupby("found_in")["value"]
-        .apply(trimmed_mean)
-        .reset_index()
-        )
-
-    reference_bins = set(trimmed_binned[trimmed_binned["value"] > 0]["found_in"].to_list())
-    return reference_bins
+    return set(reference_bins)
 
 if __name__ == "__main__":
+    os.environ["POLARS_MAX_THREADS"] = str(snakemake.threads)
+    import polars as pl
+
     binned_path = snakemake.input.appraise_binned
     unbinned_path = snakemake.input.appraise_unbinned
     genomes = snakemake.params.genomes
@@ -57,8 +81,8 @@ if __name__ == "__main__":
     sample_read = snakemake.wildcards.read
     output_path = snakemake.output
 
-    appraise_binned = pd.read_csv(binned_path, sep="\t")
-    appraise_unbinned = pd.read_csv(unbinned_path, sep="\t")
+    appraise_binned = pl.read_csv(binned_path, sep="\t")
+    appraise_unbinned = pl.read_csv(unbinned_path, sep="\t")
 
     reference_bins = pipeline(appraise_binned, appraise_unbinned, sample, MIN_APPRAISED=MIN_APPRAISED)
 
--- test/test_collect_reference_bins.py
+++ test/test_collect_reference_bins.py
@@ -1,49 +1,51 @@
 #!/usr/bin/env python3
 
 import unittest
-import pandas as pd
+import os
+os.environ["POLARS_MAX_THREADS"] = "1"
+import polars as pl
 from ibis.workflow.scripts.collect_reference_bins import pipeline
 
 APPRAISE_COLUMNS=["gene", "sample", "sequence", "num_hits", "coverage", "taxonomy", "found_in"]
 
 class Tests(unittest.TestCase):
     def test_collect_reference_bins(self):
-        appraise_binned = pd.DataFrame([
+        appraise_binned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein,genome_2_protein"],
-        ], columns=APPRAISE_COLUMNS)
-        appraise_unbinned = pd.DataFrame([
+        ], schema=APPRAISE_COLUMNS)
+        appraise_unbinned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", ""],
-        ], columns=APPRAISE_COLUMNS)
+        ], schema=APPRAISE_COLUMNS)
 
         expected = set(["genome_1", "genome_2"])
         observed = pipeline(appraise_binned, appraise_unbinned, "sample_1")
         self.assertEqual(expected, observed)
 
     def test_collect_reference_bins_no_hits(self):
-        appraise_binned = pd.DataFrame([
-        ], columns=APPRAISE_COLUMNS)
-        appraise_unbinned = pd.DataFrame([
+        appraise_binned = pl.DataFrame([
+        ], schema=APPRAISE_COLUMNS)
+        appraise_unbinned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", ""],
-        ], columns=APPRAISE_COLUMNS)
+        ], schema=APPRAISE_COLUMNS)
 
         expected = set()
         observed = pipeline(appraise_binned, appraise_unbinned, "sample_1")
         self.assertEqual(expected, observed)
 
     def test_collect_reference_bins_low_hits(self):
-        appraise_binned = pd.DataFrame([
+        appraise_binned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 1, 2, "Root", "genome_1_protein,genome_2_protein"],
-        ], columns=APPRAISE_COLUMNS)
-        appraise_unbinned = pd.DataFrame([
+        ], schema=APPRAISE_COLUMNS)
+        appraise_unbinned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 10, 20, "Root", ""],
-        ], columns=APPRAISE_COLUMNS)
+        ], schema=APPRAISE_COLUMNS)
 
         expected = set()
         observed = pipeline(appraise_binned, appraise_unbinned, "sample_1")
         self.assertEqual(expected, observed)
 
     def test_collect_reference_bins_trimmed(self):
-        appraise_binned = pd.DataFrame([
+        appraise_binned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein,genome_2_protein"],
             ["S3.2", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein"],
             ["S3.3", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein"],
@@ -54,26 +56,26 @@ class Tests(unittest.TestCase):
             ["S3.8", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein"],
             ["S3.9", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein"],
             ["S3.10", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein"],
-        ], columns=APPRAISE_COLUMNS)
-        appraise_unbinned = pd.DataFrame([
+        ], schema=APPRAISE_COLUMNS)
+        appraise_unbinned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", ""],
-        ], columns=APPRAISE_COLUMNS)
+        ], schema=APPRAISE_COLUMNS)
 
         expected = set(["genome_1"])
         observed = pipeline(appraise_binned, appraise_unbinned, "sample_1")
         self.assertEqual(expected, observed)
 
     def test_collect_reference_bins_multiple_samples(self):
-        appraise_binned = pd.DataFrame([
+        appraise_binned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", "genome_1_protein"],
             ["S3.1", "sample_2.1", "AAA", 5, 10, "Root", "genome_2_protein"],
             ["S3.1", "sample_3.1", "AAA", 5, 10, "Root", "genome_2_protein"],
-        ], columns=APPRAISE_COLUMNS)
-        appraise_unbinned = pd.DataFrame([
+        ], schema=APPRAISE_COLUMNS)
+        appraise_unbinned = pl.DataFrame([
             ["S3.1", "sample_1.1", "AAA", 5, 10, "Root", ""],
             ["S3.1", "sample_2.1", "AAA", 5, 10, "Root", ""],
             ["S3.1", "sample_3.1", "AAA", 5, 10, "Root", ""],
-        ], columns=APPRAISE_COLUMNS)
+        ], schema=APPRAISE_COLUMNS)
 
         expected = set(["genome_1"])
         observed = pipeline(appraise_binned, appraise_unbinned, "sample_1")
