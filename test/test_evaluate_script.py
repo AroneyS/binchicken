66172d4911e4092e97e01b0c33e9f567a4547053
AroneyS <samuel.aroney@qut.edu.au>
2023-03-08 14:48:57 +1000

    add duplicate sequences test from Rscript test remove Rscript test

--- test/test_evaluate_script.py
+++ test/test_evaluate_script.py
@@ -411,6 +411,80 @@ class Tests(unittest.TestCase):
         self.assertDataFrameEqual(expected_unmatched, observed_unmatched)
         self.assertDataFrameEqual(expected_summary, observed_summary)
 
+    def test_evaluate_script_duplicate_sequences(self):
+        targets = pl.DataFrame([
+            ["S3.1", "sample_1.1", "AAA", 5, 10.0, "Root; old", 10],
+            ["S3.1", "sample_3.1", "AAA", 5, 10.0, "Root; old", 10],
+            ["S3.1", "sample_1.1", "CCC", 5, 10.0, "Root; old", 11],
+            ["S3.1", "sample_2.1", "CCC", 5, 10.0, "Root; old", 11],
+            ["S3.1", "sample_1.1", "YYY", 5, 10.0, "Root; old", 98],
+            ["S3.1", "sample_2.1", "YYY", 5, 10.0, "Root; old", 98],
+            ["S3.1", "sample_2.1", "ZZZ", 5, 10.0, "Root; old", 99],
+            ["S3.1", "sample_3.1", "ZZZ", 5, 10.0, "Root; old", 99],
+        ], schema=TARGET_COLUMNS)
+        nontargets = pl.DataFrame([
+            ["S3.1", "sample_3.1", "BBB", 5, 10.0, "Root; old", "oldgenome_1"],
+            ["S3.1", "sample_1.1", "DDD", 5, 10.0, "Root; old", "oldgenome_2"],
+        ], schema=APPRAISE_COLUMNS)
+        clusters = pl.DataFrame([
+            ["sample_1,sample_2,sample_3", 3, 2, 2, 3000, "sample_1,sample_2,sample_3", "coassembly_0"],
+            ["sample_1,sample_2", 2, 2, 2, 2000, "sample_1,sample_2,sample_3", "coassembly_1"],
+        ], schema=CLUSTER_COLUMNS)
+        edges = pl.DataFrame([
+            ["Root", 1, "10", "sample_1.1", "sample_3.1"],
+            ["Root", 2, "11,98", "sample_1.1", "sample_2.1"],
+            ["Root", 1, "99", "sample_2.1", "sample_3.1"],
+        ], schema=EDGE_COLUMNS)
+        recovered = pl.DataFrame([
+            ["S3.1", "coassembly_0-genome_0_transcripts", "AAA", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_0-genome_1_transcripts", "AAA", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_0-genome_1_transcripts", "AAB", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_0-genome_1_transcripts", "BBB", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_1-genome_1_transcripts", "CCC", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_1-genome_1_transcripts", "CCD", 1, 2.0, "Root"],
+            ["S3.1", "coassembly_1-genome_1_transcripts", "DDD", 1, 2.0, "Root"],
+        ], schema=SINGLEM_COLUMNS)
+        recovered_bins = {
+            "coassembly_0-genome_0": "genome_0.fna",
+            "coassembly_0-genome_1": "genome_1.fna",
+            "coassembly_1-genome_0": "genome_0.fna",
+            "coassembly_1-genome_1": "genome_1.fna",
+            "coassembly_1-genome_2": "genome_2.fna",
+            }
+
+        expected_matches = pl.DataFrame([
+            ["coassembly_0", "S3.1", "AAA", "genome_0_transcripts", "10", None, "Root"],
+            ["coassembly_0", "S3.1", "AAA", "genome_1_transcripts", "10", None, "Root"],
+            ["coassembly_0", "S3.1", "BBB", "genome_1_transcripts", None, "oldgenome_1", "Root"],
+            ["coassembly_0", "S3.1", "CCC", None, "11", None, "Root; old"],
+            ["coassembly_0", "S3.1", "YYY", None, "98", None, "Root; old"],
+            ["coassembly_0", "S3.1", "ZZZ", None, "99", None, "Root; old"],
+            ["coassembly_1", "S3.1", "CCC", "genome_1_transcripts", "11", None, "Root"],
+            ["coassembly_1", "S3.1", "DDD", "genome_1_transcripts", None, "oldgenome_2", "Root"],
+            ["coassembly_1", "S3.1", "YYY", None, "98", None, "Root; old"],
+        ], schema=OUTPUT_COLUMNS)
+        expected_unmatched = pl.DataFrame([
+            ["coassembly_0", "S3.1", "AAB", "genome_1_transcripts", None, None, "Root"],
+            ["coassembly_1", "S3.1", "CCD", "genome_1_transcripts", None, None, "Root"],
+        ], schema=OUTPUT_COLUMNS)
+        expected_summary = pl.DataFrame([
+            ["coassembly_0", "sequences", "targets", 1, 3, 4, 25],
+            ["coassembly_0", "taxonomy", "targets", 1, 1, 2, 50],
+            ["coassembly_0", "nontarget_sequences", "recovery", 1, 3, 4, 25],
+            ["coassembly_0", "novel_sequences", "recovery", 1, 3, 4, 25],
+            ["coassembly_0", "bins", "recovery", 2, 0, 2, 100],
+            ["coassembly_1", "sequences", "targets", 1, 1, 2, 50],
+            ["coassembly_1", "taxonomy", "targets", 1, 1, 2, 50],
+            ["coassembly_1", "nontarget_sequences", "recovery", 1, 2, 3, 33.33],
+            ["coassembly_1", "novel_sequences", "recovery", 1, 2, 3, 33.33],
+            ["coassembly_1", "bins", "recovery", 1, 2, 3, 33.33],
+        ], schema=SUMMARY_COLUMNS)
+
+        observed_matches, observed_unmatched, observed_summary = evaluate(targets, nontargets, clusters, edges, recovered, recovered_bins)
+        self.assertDataFrameEqual(expected_matches, observed_matches)
+        self.assertDataFrameEqual(expected_unmatched, observed_unmatched)
+        self.assertDataFrameEqual(expected_summary, observed_summary)
+
 
 if __name__ == '__main__':
     unittest.main()
