cdc63188d1a8cbd8d0d1751e591b21badf75760d
AroneyS <samuel.aroney@qut.edu.au>
2023-05-10 15:23:50 +1000

    add more test cases

--- thread_fifo.py
+++ thread_fifo.py
@@ -1,7 +1,21 @@
 import polars as pl
 import os
+import sys
 import threading
+from multiprocessing import Process
 from bird_tool_utils import in_tempdir
+
+type = sys.argv[1]
+split = sys.argv[2]
+"""
+Testing:
+python thread_fifo.py read_csv process => BrokenPipeError/no such device
+python thread_fifo.py read_csv thread => BrokenPipeError/no such device
+python thread_fifo.py scan_csv process => OSError: Illegal seek (os error 29)
+python thread_fifo.py scan_csv thread => hangs
+python thread_fifo.py base process => works
+python thread_fifo.py base thread => works
+"""
 
 # Define a generator function that yields CSV strings
 def csv_data_generator():
@@ -19,16 +33,27 @@ def write_csv_to_fifo(fifo_path):
 
 with in_tempdir():
     # Create a named pipe using mkfifo
-    fifo_path = "my_fifo"
-    print(os.path.abspath(fifo_path))
+    fifo_path = os.path.abspath("my_fifo")
+    print(fifo_path)
     os.mkfifo(fifo_path)
 
     # Start a new thread to write CSV data to the named pipe
-    csv_writer_thread = threading.Thread(target=write_csv_to_fifo, args=(fifo_path,))
+    if split == "process":
+        csv_writer_thread = Process(target=write_csv_to_fifo, args=(fifo_path,))
+    else:
+        csv_writer_thread = threading.Thread(target=write_csv_to_fifo, args=(fifo_path,))
     csv_writer_thread.start()
 
-    # Use scan_csv to parse the CSV data from the named pipe in the main thread
-    df = pl.scan_csv(fifo_path)
+    if type == "scan_csv":
+        # Use scan_csv to parse the CSV data from the named pipe in the main thread
+        df = pl.scan_csv(fifo_path).collect()
+    elif type == "read_csv":
+        df = pl.read_csv(fifo_path)
+    else:
+        df = []
+        with open(fifo_path, "r") as f:
+            for line in f:
+                df.append(line)
 
     # Wait for the writer thread to finish
     csv_writer_thread.join()
