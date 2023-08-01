c01897f2b62de1a93259eb43a12c8b59afa6a20b
AroneyS <samuel.aroney@qut.edu.au>
2023-08-02 08:53:52 +1000

    remove pdb

--- test/test_iterate.py
+++ test/test_iterate.py
@@ -227,7 +227,6 @@ class Tests(unittest.TestCase):
             config_path = os.path.join("test", "config.yaml")
             self.assertTrue(os.path.exists(config_path))
             config = load_configfile(config_path)
-            import pdb; pdb.set_trace()
             self.assertEqual(config["genomes"], {os.path.splitext(os.path.basename(g))[0]: g.replace(MOCK_COASSEMBLE + "/coassemble/coassembly_0/recover/bins/final_bins/", os.getcwd() + "/test/recovered_bins/") for g in (GENOMES + " " + MOCK_GENOMES).split(" ")})
 
     def test_iterate_default_config(self):
