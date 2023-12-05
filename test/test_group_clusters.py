e993cd396d3332a3e275386f2afd266a94c7e638
AroneyS <samuel.aroney@qut.edu.au>
2023-12-06 08:57:59 +1000

    group targets and edges with target prefix

--- test/test_group_clusters.py
+++ test/test_group_clusters.py
@@ -37,6 +37,9 @@ class Tests(unittest.TestCase):
     def assertDataFrameEqual(self, a, b):
         assert_frame_equal(a, b, check_dtype=False)
 
+    ###################
+    ### Group edges ###
+    ###################
     def test_group_edges(self):
         edge1 = pl.DataFrame([
             ["match", 2, "sample_1,sample_2", "1,2"],
@@ -84,6 +87,33 @@ class Tests(unittest.TestCase):
         observed = edges_processing(edges, preclusters).collect()
         self.assertDataFrameEqual(expected, observed)
 
+    def test_group_two_edges(self):
+        edge1 = pl.DataFrame([
+            ["match", 2, "sample_1,sample_2", "1,2"],
+            ["pool", 3, "sample_1,sample_2,sample_3", "3,4"],
+        ], schema=EDGES_COLUMNS)
+
+        edge2 = pl.DataFrame([
+            ["match", 2, "sample_4,sample_5", "1,2"],
+            ["pool", 3, "sample_4,sample_5,sample_6", "3,4"],
+        ], schema=EDGES_COLUMNS)
+
+        edges = [edge1, edge2]
+        preclusters = ["1", "2"]
+
+        expected = pl.DataFrame([
+            ["match", 2, "sample_1,sample_2", "1_1,1_2"],
+            ["pool", 3, "sample_1,sample_2,sample_3", "1_3,1_4"],
+            ["match", 2, "sample_4,sample_5", "2_1,2_2"],
+            ["pool", 3, "sample_4,sample_5,sample_6", "2_3,2_4"],
+        ], schema=EDGES_COLUMNS)
+
+        observed = edges_processing(edges, preclusters)
+        self.assertDataFrameEqual(expected, observed)
+
+    #####################
+    ### Group targets ###
+    #####################
     def test_group_targets(self):
         target1 = pl.DataFrame([
             ["gene_1", "sample_1", "ATCG", 2, 0.5, "taxonomy_1", "1"],
@@ -131,6 +161,33 @@ class Tests(unittest.TestCase):
         observed = targets_processing(targets, preclusters).collect()
         self.assertDataFrameEqual(expected, observed)
 
+    def test_group_two_targets(self):
+        target1 = pl.DataFrame([
+            ["gene_1", "sample_1", "ATCG", 2, 0.5, "taxonomy_1", "1"],
+            ["gene_2", "sample_2", "ATCG", 3, 0.5, "taxonomy_2", "2"],
+        ], schema=TARGETS_COLUMNS)
+
+        target2 = pl.DataFrame([
+            ["gene_1", "sample_3", "ATCG", 2, 0.5, "taxonomy_1", "1"],
+            ["gene_2", "sample_4", "ATCG", 3, 0.5, "taxonomy_2", "2"],
+        ], schema=TARGETS_COLUMNS)
+
+        targets = [target1, target2]
+        preclusters = ["1", "2"]
+
+        expected = pl.DataFrame([
+            ["gene_1", "sample_1", "ATCG", 2, 0.5, "taxonomy_1", "1_1"],
+            ["gene_2", "sample_2", "ATCG", 3, 0.5, "taxonomy_2", "1_2"],
+            ["gene_1", "sample_3", "ATCG", 2, 0.5, "taxonomy_1", "2_1"],
+            ["gene_2", "sample_4", "ATCG", 3, 0.5, "taxonomy_2", "2_2"],
+        ], schema=TARGETS_COLUMNS)
+
+        observed = targets_processing(targets, preclusters)
+        self.assertDataFrameEqual(expected, observed)
+
+    ######################
+    ### Group clusters ###
+    ######################
     def test_group_clusters(self):
         cluster1 = pl.DataFrame([
             ["sample_1,sample_2", 2, 4, 3, "sample_1,sample_2,sample_3", "coassembly_0"],
