2a502fc45bf933bf82075743c2f3c98f2ff07351
AroneyS <samuel.aroney@qut.edu.au>
2023-12-12 07:33:50 +1000

    add scaled test

--- test/test_precluster_samples.py
+++ test/test_precluster_samples.py
@@ -46,6 +46,36 @@ class Tests(unittest.TestCase):
         observed = processing(unbinned, MAX_CLUSTER_SIZE=2)
         self.assertListListEqual(expected, observed)
 
+    def test_precluster_samples(self):
+        sequences = [
+            "ATGACTAGTCATAGCTAGATTTGAGGCAGCAGGAGTTAGGAAAGCCCCCGGAGTTAGCTA",
+            "TGACTAGCTGGGCTAGCTATATTCTTTTTACGAGCGCGAGGAAAGCGACAGCGGCCAGGC",
+            "ATCGACTGACTTGATCGATCTTTGACGACGAGAGAGAGAGCGACGCGCCGAGAGGTTTCA",
+            "TACGAGCGGATCGTGCACGTAGTCAGTCGTTATATATCGAAAGCTCATGCGGCCATATCG",
+            "ATCATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGA",
+            "GATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATC",
+        ]
+        # large group of 99 samples with 3 in common
+        large = [
+            ["S3.1", f"sample_{i}", s, 5, 10, "Root"] for s in sequences[0:3] for i in range(1, 100)
+        ]
+        # another group of 99 samples with 3 in common (2 overlap above)
+        overlap = [
+            ["S3.1", f"sample_{i}", s, 5, 10, "Root"] for s in sequences[1:4] for i in range(100, 199)
+        ]
+        # another group of 20 samples with 2 in common (independent)
+        small = [
+            ["S3.1", f"sample_{i}", s, 5, 10, "Root"] for s in sequences[4:6] for i in range(199, 218)
+        ]
+
+        unbinned = pl.DataFrame(large + overlap + small, schema=OTU_TABLE_COLUMNS)
+
+        observed = processing(unbinned, MAX_CLUSTER_SIZE=100)
+        largest_observed = max(observed, key=len)
+        smallest_observed = min(observed, key=len)
+        self.assertTrue(len(largest_observed) <= 1000)
+        self.assertTrue(len(smallest_observed) >= 10)
+
 
 if __name__ == '__main__':
     unittest.main()
