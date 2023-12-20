6bafe0ac6fbb780acf24f1517aba117f3c32d878
AroneyS <samuel.aroney@qut.edu.au>
2023-12-20 13:24:03 +1000

    remove group-components: have to specify per group

--- ibis/ibis.py
+++ ibis/ibis.py
@@ -98,7 +98,7 @@ def run_workflow(config, workflow, output_dir, cores=16, dryrun=False,
 
     cmd = (
         "snakemake --snakefile {snakefile} --configfile '{config}' --directory {output_dir} "
-        "{jobs} --rerun-incomplete --keep-going --nolock --group-components 10 "
+        "{jobs} --rerun-incomplete --keep-going --nolock "
         "{snakemake_args} "
         "{profile} {local} {retries} --use-conda {conda_frontend} {conda_prefix} "
         "{dryrun} "
