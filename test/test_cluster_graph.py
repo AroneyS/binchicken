eccc795e24aa4374ca7e3a97c77d95119a4d892b
AroneyS <samuel.aroney@qut.edu.au>
2024-03-06 13:07:14 +1000

    improve test reproducibility

--- test/test_cluster_graph.py
+++ test/test_cluster_graph.py
@@ -586,6 +586,7 @@ class Tests(unittest.TestCase):
     def test_cluster_restrict_coassembly_samples_changed_len(self):
         elusive_edges = pl.DataFrame([
             ["pool", 3, "1,2,3,4,5,6", "1,3"],
+            ["pool", 3, "1,2,3,4", "4"],
             ["pool", 3, "1,2,3", "6"],
         ], schema = ELUSIVE_EDGES_COLUMNS)
         read_size = pl.DataFrame([
@@ -598,7 +599,7 @@ class Tests(unittest.TestCase):
         ], schema=READ_SIZE_COLUMNS)
 
         expected = pl.DataFrame([
-            ["1,2,3", 3, 3, 6000, "1,2,3,4", "coassembly_0"],
+            ["1,2,3", 3, 4, 6000, "1,2,3,4", "coassembly_0"],
         ], schema=ELUSIVE_CLUSTERS_COLUMNS)
         observed = pipeline(
             elusive_edges,
