92ba148265758d54f09034113598b4f0a9b26dbc
AroneyS <samuel.aroney@qut.edu.au>
2024-07-16 14:26:01 +1000

    add abundance_weighting script

---
+++ binchicken/workflow/scripts/abundance_weighting.py
@@ -0,0 +1,68 @@
+##############################
+### abundance_weighting.py ###
+##############################
+# Author: Samuel Aroney
+
+import os
+import polars as pl
+import logging
+
+APPRAISE_COLUMNS = {
+    "gene": str,
+    "sample": str,
+    "sequence": str,
+    "num_hits": int,
+    "coverage": float,
+    "taxonomy": str,
+    "found_in": str,
+}
+
+WEIGHTING_COLUMNS = {
+    "gene": str,
+    "sequence": str,
+    "weight": float,
+}
+
+def pipeline(unbinned, binned):
+    logging.info(f"Polars using {str(pl.thread_pool_size())} threads")
+
+    if len(unbinned) == 0:
+        logging.warning("No unbinned sequences found")
+        return pl.DataFrame(schema=WEIGHTING_COLUMNS)
+
+    total_coverage = (
+        pl.concat([binned, unbinned])
+        .group_by("sample")
+        .agg(total_coverage = pl.sum("coverage"))
+    )
+
+    weighted = (
+        unbinned
+        .join(total_coverage, on="sample")
+        .with_columns(weight = pl.col("coverage") / pl.col("total_coverage"))
+        .group_by(["gene", "sequence"])
+        .agg(pl.mean("weight"))
+    )
+
+    return weighted
+
+if __name__ == "__main__":
+    os.environ["POLARS_MAX_THREADS"] = str(snakemake.threads)
+    import polars as pl
+
+    logging.basicConfig(
+        filename=snakemake.log[0],
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s: %(message)s',
+        datefmt='%Y/%m/%d %I:%M:%S %p'
+        )
+
+    unbinned_path = snakemake.input.unbinned
+    binned_path = snakemake.input.binned
+    weighted_path = snakemake.output.weighted
+
+    unbinned = pl.read_csv(unbinned_path, separator="\t")
+    binned = pl.read_csv(binned_path, separator="\t")
+
+    weighted = pipeline(unbinned, binned)
+    weighted.write_csv(weighted_path, separator="\t")
