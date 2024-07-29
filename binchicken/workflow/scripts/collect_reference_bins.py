Fix abundance weighting (#132)
* fix deprecated dtypes arg

* add suffix removal to abundance weighting

* move suffix regex to binchicken.py

* error when weightings are empty

* move unbinned filtering within samples if

* average properly over samples without the sequence

* fix tests

* fix more tests

* remove absent sequences from output

* fix test

binchicken/workflow/scripts/collect_reference_bins.py
@@ -7,6 +7,7 @@ import polars as pl
 import os
 import numpy as np
 import extern
+from binchicken.binchicken import SUFFIX_RE
 
 def trimmed_mean(data, trim=0.1):
     cut = int(np.floor(len(data) * trim))
@@ -23,7 +24,7 @@ def pipeline(appraise_binned, appraise_unbinned, sample, MIN_APPRAISED=0.1, TRIM
         appraise_binned
         .with_columns(
             pl.col("sample").cast(str),
-            sample_remove_suffix = pl.col("sample").str.replace(r"(_|\.)R?1$", ""),
+            sample_remove_suffix = pl.col("sample").str.replace(SUFFIX_RE, ""),
             )
         .filter((pl.col("sample") == sample) | (pl.col("sample_remove_suffix") == sample))
     )
@@ -32,7 +33,7 @@ def pipeline(appraise_binned, appraise_unbinned, sample, MIN_APPRAISED=0.1, TRIM
         appraise_unbinned
         .with_columns(
             pl.col("sample").cast(str),
-            sample_remove_suffix = pl.col("sample").str.replace(r"(_|\.)R?1$", ""),
+            sample_remove_suffix = pl.col("sample").str.replace(SUFFIX_RE, ""),
             )
         .filter((pl.col("sample") == sample) | (pl.col("sample_remove_suffix") == sample))
     )

binchicken/workflow/scripts/target_elusive.py
@@ -9,6 +9,7 @@ import logging
 import numpy as np
 import scipy.sparse as sp
 import itertools
+from binchicken.binchicken import SUFFIX_RE
 
 EDGES_COLUMNS={
     "style": str,
@@ -130,7 +131,7 @@ def streaming_pipeline(
         .with_columns(
             pl.when(pl.col("sample").is_in(samples))
             .then(pl.col("sample"))
-            .otherwise(pl.col("sample").str.replace(r"(_|\.)R?1$", ""))
+            .otherwise(pl.col("sample").str.replace(SUFFIX_RE, ""))
             )
         .filter(pl.col("sample").is_in(samples))
         .drop("found_in")
@@ -216,7 +217,7 @@ def pipeline(
         .with_columns(
             pl.when(pl.col("sample").is_in(samples))
             .then(pl.col("sample"))
-            .otherwise(pl.col("sample").str.replace(r"(_|\.)R?1$", ""))
+            .otherwise(pl.col("sample").str.replace(SUFFIX_RE, ""))
             )
         .filter(pl.col("sample").is_in(samples))
         .drop("found_in")