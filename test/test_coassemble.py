b11092295b010d48f150addf9e2474bbb5970331
AroneyS <samuel.aroney@qut.edu.au>
2025-08-06 13:35:26 +1000

    add test for different naming schemes

--- test/test_coassemble.py
+++ test/test_coassemble.py
@@ -2311,5 +2311,91 @@ class Tests(unittest.TestCase):
             with open(cluster_path) as f:
                 self.assertEqual(expected, f.read())
 
+    def test_coassemble_sample_naming(self):
+        with in_tempdir():
+            local_samples_forward = [
+                os.path.abspath("sample_1.1.fq"),
+                os.path.abspath("sample_2_R1.fq"),
+                os.path.abspath("sample_3_1.fq"),
+                os.path.abspath("sample_R1_4.fq"),
+            ]
+            for old, new in zip(SAMPLE_READS_FORWARD_EMPTY.split(" "), local_samples_forward):
+                shutil.copy(old, new)
+
+            local_samples_reverse = [
+                os.path.abspath("sample_1.2.fq"),
+                os.path.abspath("sample_2_R2.fq"),
+                os.path.abspath("sample_3_2.fq"),
+                os.path.abspath("sample_R2_4.fq"),
+            ]
+            for old, new in zip(SAMPLE_READS_REVERSE_EMPTY.split(" "), local_samples_reverse):
+                shutil.copy(old, new)
+
+            cmd = (
+                f"binchicken coassemble "
+                f"--forward {' '.join(local_samples_forward)} "
+                f"--reverse {' '.join(local_samples_reverse)} "
+                f"--genomes {TWO_GENOMES} "
+                f"--singlem-metapackage {METAPACKAGE} "
+                f"--prodigal-meta "
+                f"--output test "
+            )
+            extern.run(cmd)
+
+            config_path = os.path.join("test", "config.yaml")
+            self.assertTrue(os.path.exists(config_path))
+
+            sample_1_path = os.path.join("test", "coassemble", "pipe", "sample_1_read.otu_table.tsv")
+            self.assertTrue(os.path.exists(sample_1_path))
+
+            sample_2_path = os.path.join("test", "coassemble", "pipe", "sample_2_read.otu_table.tsv")
+            self.assertTrue(os.path.exists(sample_2_path))
+
+            sample_3_path = os.path.join("test", "coassemble", "pipe", "sample_3_read.otu_table.tsv")
+            self.assertTrue(os.path.exists(sample_3_path))
+
+            sample_4_path = os.path.join("test", "coassemble", "pipe", "sample_read.otu_table.tsv")
+            self.assertTrue(os.path.exists(sample_4_path))
+
+            read_size_path = os.path.join("test", "coassemble", "read_size.csv")
+            self.assertTrue(os.path.exists(read_size_path))
+            expected = "\n".join(
+                [
+                    ",".join(["sample_1", "4832"]),
+                    ",".join(["sample_2", "3926"]),
+                    ",".join(["sample_3", "3624"]),
+                    ",".join(["sample", "604"]),
+                    ""
+                ]
+            )
+            with open(read_size_path) as f:
+                self.assertEqual(expected, f.read())
+
+            cluster_path = os.path.join("test", "coassemble", "target", "elusive_clusters.tsv")
+            self.assertTrue(os.path.exists(cluster_path))
+            expected = "\n".join(
+                [
+                    "\t".join([
+                        "samples",
+                        "length",
+                        "total_targets",
+                        "total_size",
+                        "recover_samples",
+                        "coassembly",
+                    ]),
+                    "\t".join([
+                        "sample_1,sample_2",
+                        "2",
+                        "3",
+                        "8758",
+                        "sample_1,sample_2,sample_3",
+                        "coassembly_0"
+                    ]),
+                    ""
+                ]
+            )
+            with open(cluster_path) as f:
+                self.assertEqual(expected, f.read())
+
 if __name__ == '__main__':
     unittest.main()
